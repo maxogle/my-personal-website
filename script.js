620ab74ba3d22ab0ee1c2555d3ed1bf99b6cec3e
alexandermaxwellogle <maxogle@gmail.com>
2022-02-28 13:02:24 -0500

    updated css, Js functions, added body test and buttons in HTML.

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-const h2 = documnet.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+function alert1 (){
+    alert("Nice try! I can see you dont have the funds. ;)")
+
+}
+
+function alert2 (){
+    alert("You better be President and it better be a FANTASTIC horse!")
+}
+
+function alert3 (){
+    alert("Extremly rude...please stop.")
+}
+
+button1.onclick = alert1();
+
+button2.onclick = alert2();
+
+button3.onclick = alert3();
